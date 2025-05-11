dc5dc58399ae15b70de438a8cf0c43ff9aa8dba7
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-05-11 16:22:16 +0600

    #Update1.00 setup allfiles

---
+++ src/components/home/home.jsx
@@ -0,0 +1,38 @@
+"use client";
+import { useState } from "react";
+import styles from "./home.module.css";
+
+const Home = () => {
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSetCookie = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("http://localhost:1618/", {
+        credentials: "include",
+      });
+      const getData = await response.json();
+      setData(getData);
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  console.log(data);
+  return (
+    <section className={styles.home}>
+      {loading ? (
+        <h2>Loading</h2>
+      ) : (
+        <>
+          <h1>Hi</h1>
+          <button onClick={handleSetCookie}>save cookie</button>
+        </>
+      )}
+    </section>
+  );
+};
+
+export default Home;
