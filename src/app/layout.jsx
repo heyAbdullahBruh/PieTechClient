72e7e25cd769c905d5b87defc7e933ef798bd569
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-05-12 20:19:02 +0600

    #Update1.07 footer add

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navber from "@/components/navber/navber";
+import Footer from "@/components/Footer/foooter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,7 @@ export default function RootLayout({ children }) {
           <Navber />
         </nav>
         <main>{children}</main>
-        <footer>Foo</footer>
+        <footer><Footer/></footer>
       </body>
     </html>
   );
---
+++ src/components/Footer/foooter.jsx
@@ -0,0 +1,99 @@
+"use client";
+import Link from "next/link";
+import styles from "./footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagramSquare,
+  faLinkedinIn,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faArrowUp, faEarthAmericas } from "@fortawesome/free-solid-svg-icons";
+
+const Footer = () => {
+  return (
+    <section className={styles.footer}>
+        <Link href={'#'} className={styles.arrowUp}><button><FontAwesomeIcon icon={faArrowUp}/></button></Link>
+      <div className={styles.footerUP}>
+        <ul>
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <span>|</span>
+          <li>
+            <Link href={"/services"}>Services</Link>
+          </li>
+          <span>|</span>
+          <li>
+            <Link href={"/works"}>Works</Link>
+          </li>
+          <span>|</span>
+          <li>
+            <Link href={"/articles"}>Article</Link>
+          </li>
+          <span>|</span>
+          <li>
+            <Link href={"/about-us"}>About-US</Link>
+          </li>
+          <span>|</span>
+          <li>
+            <Link href={"/about-team"}>Our Team</Link>
+          </li>
+          <span>|</span>
+          <li>
+            <Link href={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+      </div>
+      <hr />
+      <div className={styles.footerMid}>
+        <Link href={"#"}>
+          <h1 className={styles.logo}>PieTech.io</h1>
+          <p>We Build Better.</p>
+        </Link>
+        <p>
+          <strong>Address : </strong>Agarogoan, Dhaka, Bangladesh
+        </p>
+        <p>
+          <strong>Phone :</strong> +8801603070892
+        </p>
+        <p>
+          <strong>Email : </strong>
+          <Link href={"mailto:pietech1618@protonmail.com"}>
+            pietech1618@protonmail.com
+          </Link>
+        </p>
+      </div>
+      <hr />
+      <div className={styles.footerDown}>
+        <Link href={""}>
+          <button>
+            <FontAwesomeIcon icon={faFacebook} />
+          </button>
+        </Link>
+        <Link href={""}>
+          <button>
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </button>
+        </Link>
+        <Link href={""}>
+          <button>
+            <FontAwesomeIcon icon={faInstagramSquare} />
+          </button>
+        </Link>
+        <Link href={""}>
+          <button>
+            <FontAwesomeIcon icon={faXTwitter} />
+          </button>
+        </Link>
+        <Link href={"#"}>
+          <button>
+            <FontAwesomeIcon icon={faEarthAmericas} />
+          </button>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
