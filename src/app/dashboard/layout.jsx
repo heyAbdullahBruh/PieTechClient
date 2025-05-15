a8ac785820d4f759d0c967a9c5f6c2613ebb35d8
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-05-15 20:08:29 +0600

    #Update1.15 add dashboard context

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,5 +1,8 @@
 import DashboardAuth from "@/components/dashboard/AuthDash/DashboardAuth";
+import DashProvider from "@/components/dashboard/DashCotext/DashContext";
 import DashboardNav from "@/components/dashboard/navDash";
+import { api } from "@/data/api";
+import { cookies } from "next/headers";
 
 export const metadata = {
   title: "PieTech — Dashboard",
@@ -7,12 +10,34 @@ export const metadata = {
     "PieTech is an smart digital agency , whose solve digital problem for everyone",
 };
 
-export default function DashBoardLayout({ children }) {
-  return (
-    <aside>
-        <DashboardNav/>
-        <DashboardAuth/>
-        <main>{children}</main>
-    </aside>
-  );
+export default async function DashBoardLayout({ children }) {
+  try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value;
+
+    const response = await fetch(`${api}/admin/authPermission`, {
+      method: "GET",
+      headers: {
+        cookie: `token=${token}`, // manually forward token
+      },
+      cache: "no-store", // avoid caching for SSR
+    });
+    const getData = await response.json();
+    // console.log(getData);
+    return (
+      <DashProvider authPermission={getData?.auth}>
+        {getData?.auth === true ? (
+          <>
+            <DashboardNav />
+
+            <main>{children}</main>
+          </>
+        ) : (
+          <DashboardAuth />
+        )}
+      </DashProvider>
+    );
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
---
+++ src/components/dashboard/DashCotext/DashContext.jsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { api } from "@/data/api";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const DashContext = createContext();
+
+const DashProvider = ({ children,authPermission }) => {
+  const [accessToken, setAccessToken] = useState("");
+  const [auth, setAuth] = useState(false);
+
+  //   Refresh for access token get -->
+  const refresh = async () => {
+    try {
+      const response = await fetch(`${api}/admin/accessToken`, {
+        method: "GET",
+        credentials: "include",
+      });
+      const getData = await response.json();
+
+      if (getData?.success) {
+        setAccessToken(getData?.token);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  useEffect(() => {
+    refresh();
+    setAuth(authPermission)
+  }, []);
+//   console.log({accessToken,auth});
+  return (
+    <DashContext.Provider value={{ accessToken, setAccessToken, auth }}>
+      {children}
+    </DashContext.Provider>
+  );
+};
+
+export const useDashAuth = () => {
+  const context = useContext(DashContext);
+  if (!context) {
+    throw new Error("useAuth must be used within a DashProvider");
+  }
+  return context;
+};
+export default DashProvider;
